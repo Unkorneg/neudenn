cb887cc7f1efa19930dde8eb6f2fcfb28de77bd3
unkorneg <unkorneg@gozmail.bzh>
2022-01-27 19:02:03 +0100

    tuto templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+
+# from flask_sqlalchemy import SQLAlchemy
 
 # create database connection object
-db = SQLAlchemy()
+# db = SQLAlchemy()
 
+app = Flask(__name__)
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object("config.Config")
-
-    db.init_app(app)
-
-    # a simple page that says hello
-    @app.route("/hello")
-    def hello():
-        return "Hello, World!"
-
-    return app
+from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,25 @@
-from flask import Flask
+from flask import render_template
+from app import app
 
 
 @app.route("/")
+@app.route("/index")
 def index():
-    return "Hello world !"
+    user = {"username": "Dewi"}
+    posts = [
+        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
+        {
+            "author": {"username": "Susan"},
+            "body": "The Avengers movie was so cool!",
+        },
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "This is the about page"
+    user = {"username": "Dewi"}
+    return render_template("about.html", user=user)
 
 
 if __name__ == "__main__":
